c641cfb453f859d3b8341240c7848fa63d5adb18
Nikhi-ghub <iamnikhilai@gmail.com>
2026-01-06 22:06:25 +0530

    loops.c

---
+++ loops.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+voidprintBinary(int n){
+  if(n==o);
+  return;
+}
+  int binsry[32],i=0;
+  while(n>=0){
+  binary[i]=n%2;
+  n=n/2;
+  i++;
+  }
+for(int j=1:j>=0;j--)
+  pritnf("%d",binary[j]);
+}
+int main(){
+  int num=5;
+  printf("Input:%d\nnOutput:",num)
+  printBinary(num);
+  return0;
+}
