fd24a3b6d221d682f2d89f5d4ce53312e2ecd5c3
Nikhi-ghub <iamnikhilai@gmail.com>
2026-01-11 14:41:13 +0530

    GCD.c

--- GCD.c
+++ GCD.c
@@ -4,7 +4,7 @@ if (b == 0) return a;
 return findGCD(b, a % b);
 }
 int main() { 
-int a = 56, b = 98;
+int a = 48, b =86;
 printf("Input: a=%d, b=%d\nOutput: GCD is %d", a, b, findGCD(a, b));
 return 0;
 }
